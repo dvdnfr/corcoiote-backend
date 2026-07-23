908f9ac909eede6b63c10cc967acd3151aaf5c9a
David Nicolas <davidnicolas1008@gmail.com>
2026-07-23 15:23:10 -0300

    feat: adicinar classe para validacao de erros de campo na estrutura condicional

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,18 @@
+import type { NextFunction, Request, Response } from "express";
+import { NotFoundError, ValidationError } from "../errors/index.ts";
+
+export default function errorHandler(
+    error: unknown,
+    _request: Request,
+    response: Response,
+    _next: NextFunction
+) {
+    if (error instanceof NotFoundError || error instanceof ValidationError){
+        response.status(error.statusCode).json({message: error.statusCode});
+        return;
+    }
+
+    console.log(error); 
+
+    response.status(500).json({message:'Erro interno do servidor.'})
+}
